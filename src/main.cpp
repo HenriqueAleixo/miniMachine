90d830bee038628dc46e63ff86c38c71e91c5631
Henrique <aleixohenrique@htmail.com>
2025-02-07 10:56:00 -0300

    up 07/02/2025 10:55

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,7 @@
 #include <WebServer.h>
 #include <ArduinoOTA.h>
 
-#define PWM_CHANNEL_A 0
-#define PWM_CHANNEL_B 1
-#define PWM_FREQ 5000
-#define PWM_RESOLUTION 8
-
 // Definições dos pinos do ESP32
-int PWM_A = -1;
-int PWM_B = -1;
 int DIR_A = 5;
 int DIR_B = 18;
 int TA = 19; 
@@ -20,16 +13,8 @@ const int ledPin = 2;
 const int wifiLedPin = 4;  
 
 String command;
-int SPEED = 255;        
-int speed_Coeff = 1;
 
 WebServer server(80);
-
-unsigned long previousMillis = 0;
-
-String sta_ssid = "SEU_SSID";      
-String sta_password = "SUA_SENHA";  
-
 
 void HTTP_handleRoot() {
   server.send(200, "text/html", "");
@@ -69,7 +54,6 @@ void TurnLeft() {
 }
 
 void ForwardLeft() {
-  //frente esquerda
   digitalWrite(DIR_A, LOW);
   digitalWrite(DIR_B, LOW);
   digitalWrite(TA, LOW);
@@ -86,7 +70,6 @@ void ForwardRight() {
 }
 
 void BackwardLeft() {
-//tras esquerda
   digitalWrite(DIR_A, LOW);
   digitalWrite(DIR_B, HIGH);
   digitalWrite(TA, LOW);
@@ -94,7 +77,7 @@ void BackwardLeft() {
 }
 
 void BackwardRight() {
-    digitalWrite(DIR_A, LOW);
+  digitalWrite(DIR_A, LOW);
   digitalWrite(DIR_B, LOW);
   digitalWrite(TA, HIGH);
   digitalWrite(TB, LOW);
